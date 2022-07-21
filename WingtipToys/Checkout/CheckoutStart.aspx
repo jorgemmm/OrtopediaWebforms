ef7c4e16d7aa450fc3534ed7a04b8473531ae235
VicAgent <vj_plasin@hotmail.com>
2022-07-21 20:24:02 +0100

    aplicación aspnet 4.5 2013 store front web online to sell item with paypal

---
+++ WingtipToys/Checkout/CheckoutStart.aspx
@@ -0,0 +1,7 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="CheckoutStart.aspx.cs" Inherits="WingtipToys.Checkout.CheckoutStart" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="MainContent" runat="server">
+
+
+    <asp:TextBox runat="server"   ID="txtDiag"> </asp:TextBox>
+
+</asp:Content>
---
+++ WingtipToys/Checkout/CheckoutStart.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WingtipToys.Checkout
+{
+  public partial class CheckoutStart : System.Web.UI.Page
+  {
+    protected void Page_Load(object sender, EventArgs e)
+    {
+      NVPAPICaller payPalCaller = new NVPAPICaller();
+      string retMsg = "";
+      string token = "";
+
+            try
+            {
+                if (Session["payment_amt"] != null)
+                {
+                    string amt = Session["payment_amt"].ToString();
+
+                    bool ret = payPalCaller.ShortcutExpressCheckout(amt, ref token, ref retMsg);
+                    if (ret)
+                    {
+                        Session["token"] = token;
+                        Response.Redirect(retMsg);
+                    }
+                    else
+                    {
+                        Response.Redirect("CheckoutError.aspx?" + retMsg);
+                    }
+                }
+                else
+                {
+                    Response.Redirect("CheckoutError.aspx?ErrorCode=AmtMissing");
+                }
+            }
+            catch (Exception exc)
+            {
+                txtDiag.Text = exc.Message;
+
+                Response.Redirect("CheckoutError.aspx?exc=txtDiag.Text");
+            }
+      
+    }
+
+
+
+    }
+}
